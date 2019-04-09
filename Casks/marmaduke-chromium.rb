5aa9b56db6073bb43408240bd5ca1fb9cd4b986b
William Luke <williamluke@silverknoll.net>
2019-04-09 13:16:31 -0500

    Update marmaduke-chromium.rb

--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -3,7 +3,7 @@ cask 'marmaduke-chromium' do
   sha256 '633d6b05b200a6365f744a0c30191c29cef2102310bc3c7cbce53bac8a5e4786'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-macOS/Chromium.#{version}.app.sync.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
